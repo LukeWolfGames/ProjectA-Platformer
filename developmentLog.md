ddfb379d6aecc47e7a8be7a52f2dd297f3c42efa
Luke Wolf <lukewolfgames@gmail.com>
2019-12-04 10:01:19 +1100

    Changed project to Dude Platformer Tutorial ("Making your first Phaser 3 game" and fixed a problem with Mo's code.

--- developmentLog.md
+++ developmentLog.md
@@ -5,3 +5,9 @@
 2. Created index.html.
 3. Inserted phaser.js
 4. Created code to show Phaser icon and animation work on a page
+
+**04/12/2019**
+# CHANGE: Mo changed the tutorial 
+# HAD PROBLEM WITH GRAVITY AND JUMPING.
+# NOTE TO SELF: CHECK EVERY LINE, MAKE SURE THAT BRACKETS AND EVERY LINE IS THE SAME.
+1. Changed entire project tutorial to the "dude" tutorial and fixed issue with arcade inside var config {}. There were too many brackets.
--- game.js
+++ game.js
@@ -1,47 +1,142 @@
-var config = { 
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: "arcade",  
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create
-    }    
-}
+var config = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+				gravity: { y: 300 },
+				debug: false
+		}
+	},
+	scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
+};
+
+var player;
+var stars;
+var platforms;
+var cursors;
 
 var game = new Phaser.Game(config);
 
-function preload()
-{
-    this.load.setBaseURL("http://labs.phaser.io");
-
-    this.load.image("sky", "assets/skies/space3.png");
-    this.load.image("logo", "assets/sprites/phaser3-logo.png");
-    this.load.image("red", "assets/particles/red.png");    
+function preload() {
+	this.load.image('sky', 'assets/sky.png');
+	this.load.image('ground', 'assets/platform.png');
+	this.load.image('star', 'assets/star.png');
+	this.load.image('bomb', 'assets/bomb.png');
+	this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
-function create()
+
+
+function create() {
+	// sky
+	this.add.image(400, 300, 'sky');
+
+	// platforms
+	// grouping the platforms together
+	platforms = this.physics.add.staticGroup();
+	
+	platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+	platforms.create(600, 400, 'ground');
+	platforms.create(50, 250, 'ground');
+	platforms.create(750, 220, 'ground');
+
+
+
+	// adding the player sprite
+	player = this.physics.add.sprite(100, 450, 'dude');
+
+	// PLAYER PROPERTY SETTINGS
+	// setting the bouncing properties for the player's jumping appropriate.
+	// and setting the boundary for the player to remain within the game.
+	player.setBounce(0.2);
+	player.setCollideWorldBounds(true);
+	
+	// PLAYER ANIMATIONS
+	// player left animation
+	this.anims.create({
+		key: 'left',
+		frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+		frameRate: 10,
+		repeat: -1
+	});
+
+	// player turn animation
+	this.anims.create({
+		key: 'turn',
+		frames: [ { key: 'dude', frame: 4 } ],
+		frameRate: 20
+	});
+
+	// player right animation
+	this.anims.create({
+		key: 'right',
+		frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+		frameRate: 10,
+		repeat: -1
+	});
+
+	// CREATING PLAYER CONTROLS
+	cursors = this.input.keyboard.createCursorKeys();
+	
+	// STARS PROPERTY SETTINGS
+	// creating multiple stars as a group and setting their position.
+	stars = this.physics.add.group({
+		key: "star",
+		repeat: 11,
+		setXY: { x: 12, y: 0, stepX: 70 }
+	});
+
+	stars.children.iterate(function (child) {
+		child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+	});
+		
+	this.physics.add.collider(player, platforms);
+	this.physics.add.collider(stars, platforms);
+
+	this.physics.add.overlap(player, stars, collectStar, null, this);
+
+
+	
+
+	// setting collider for star object.	
+
+}
+
+
+function update() {
+	if (cursors.left.isDown)
+	{
+		player.setVelocityX(-160);
+
+		player.anims.play("left", true);
+	}
+	else if (cursors.right.isDown)
+	{
+		player.setVelocityX(160);
+
+		player.anims.play("right", true);
+	}
+	else
+	{
+		player.setVelocityX(0);
+
+		player.anims.play("turn");
+	}
+
+	if (cursors.up.isDown && player.body.touching.down)
+	{
+		player.setVelocityY(-330);
+	}
+}
+
+function collectStar (player, star)
 {
-    this.add.image(400, 300, "sky");
-
-    var particles = this.add.particles("red");
-    
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: {start: 1, end: 0},
-        blendMode: "ADD"
-    });
-    
-    var logo = this.physics.add.image(400, 100, "logo");
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+	star.disableBody(true, true);
 }
